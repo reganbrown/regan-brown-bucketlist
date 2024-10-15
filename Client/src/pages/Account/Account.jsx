9d6a448a97ee425281d4a1a08f463298568f805d
reganbrown <regan_brown@icloud.com>
2024-10-15 15:23:41 -0300

    add expense and savings routes

--- Client/src/pages/Account/Account.jsx
+++
@@ -1,8 +0,0 @@
-export default function Account() {
-  return (
-    <>
-      <h1>Account</h1>
-      <p>Sign Up / Sign In</p>
-    </>
-  );
-}
--- Client/src/pages/Home/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1>Home</h1>
-      <p>Add home page</p>
-    </>
-  );
-}
